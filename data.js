d68b7afd03b53ac1d1d45a2cb2c753f9cd410a0b
EA Hospitality Pulse <ogcollyns@gmail.com>
2026-08-11 09:28:41 +0000

    Auto-build: 2026-08-11 09:28 UTC

--- data.js
+++ data.js
@@ -1816,4 +1816,4 @@ window.INSIGHTS = [
   "editionKey": "evening"
  }
 ];
-window.BUILT_AT = "2026-08-11 12:27";
+window.BUILT_AT = "2026-08-11 09:28";
--- ledger.js
+++ ledger.js
@@ -1,5 +1,5 @@
 window.LEDGER = {
- "updated": "2026-08-11 12:27",
+ "updated": "2026-08-11 09:28",
  "stats": {
   "total": 69,
   "open": 62,
